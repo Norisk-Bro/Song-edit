bc6c5c23b84c5a28ab1efbba947ca663ad6b70f4
༒⃝⃝༆Sᴀᴊɪʙ❤️⃟⃟⃟⃟⃟℠⃟⃟༒ <sajibmeta1day@gmail.com>
2026-06-25 23:45:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -142,7 +142,7 @@ function startCountdown(onDone){
     s--; numEl.textContent=s;
     arc.setAttribute('stroke-dashoffset',(total/40)*(40-s));
     if(s<=0){ clearInterval(iv); onDone(); }
-  },350);
+  },1000);
 }
 
 function bypassSite(domain,cb){
